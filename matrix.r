f07cdd9bf70a236884068213c4d7c327d46c22c8
JingshuZ <91241158+mmmzzss@users.noreply.github.com>
2022-08-03 11:03:10 +0800

    Update matrix.r

--- matrix.r
+++ matrix.r
@@ -2,10 +2,12 @@
 matrix(1:9, byrow = TRUE, nrow = 3)
 
 # In the matrix() function:
-
 # The first argument is the collection of elements that R will arrange into the rows and columns of the matrix. Here, we use 1:9 which is a shortcut for c(1, 2, 3, 4, 5, 6, 7, 8, 9).
 # The argument byrow indicates that the matrix is filled by the rows. If we want the matrix to be filled by the columns, we just place byrow = FALSE.
 # The third argument nrow indicates that the matrix should have three rows.
+
+
+
 
 
 # 创建三个vector -- combine -- assign to a matrix
@@ -19,3 +21,31 @@ box_office <- c(new_hope, empire_strikes, return_jedi)
 
 # Construct star_wars_matrix（命名）
 star_wars_matrix <- matrix(box_office, byrow = TRUE, nrow = 3)
+
+
+
+
+# 给行&列命名
+# Box office Star Wars (in millions!)
+new_hope <- c(460.998, 314.4)
+empire_strikes <- c(290.475, 247.900)
+return_jedi <- c(309.306, 165.8)
+
+# Construct matrix
+star_wars_matrix <- matrix(c(new_hope, empire_strikes, return_jedi), nrow = 3, byrow = TRUE)
+
+# Vectors region and titles, used for naming
+region <- c("US", "non-US")
+titles <- c("A New Hope", "The Empire Strikes Back", "Return of the Jedi")
+
+# Name the columns with region
+colnames(star_wars_matrix) <- region 
+
+# Name the rows with titles
+rownames(star_wars_matrix) <- titles
+
+# Print out star_wars_matrix
+star_wars_matrix
+
+
+
