035a635614f958a76c6cc5f3e28c548c667f9b7a
JingshuZ <91241158+mmmzzss@users.noreply.github.com>
2022-08-06 10:34:07 +0800

    Update Language of data.r

--- Intro to Data in R/Language of data.r
+++ Intro to Data in R/Language of data.r
@@ -162,3 +162,11 @@ ggplot(email50_fortified, aes(x = number_yn)) +
   geom_bar()
 
 
+
+# Load ggplot2
+library(ggplot2)
+
+# Scatterplot of exclaim_mess vs. num_char 这里spam是一个numerical data，但是color要求是categorical，所以用factor changes it
+ggplot(email50, aes(x = num_char , y = exclaim_mess, color = factor(spam))) +
+  geom_point()
+
