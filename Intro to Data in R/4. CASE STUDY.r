3960c6400dc2e08991b72b143c928d4808d53430
JingshuZ <91241158+mmmzzss@users.noreply.github.com>
2022-08-11 09:01:49 +0800

    Update 4. CASE STUDY.r

--- Intro to Data in R/4. CASE STUDY.r
+++ Intro to Data in R/4. CASE STUDY.r
@@ -1,5 +1,5 @@
 # A tibble: 463 x 21
-   score rank  ethnicity gender language   age cls_perc_eval cls_did_eval
+   score rank  ethnicity gender language   age     cls_perc_eval  cls_did_eval
    <dbl> <fct> <fct>     <fct>  <fct>    <int>         <dbl>        <int>
  1   4.7 tenu~ minority  female english     36          55.8           24
  2   4.1 tenu~ minority  female english     36          68.8           86
@@ -8,10 +8,13 @@
  5   4.6 tenu~ not mino~ male   english     59          85             17
  6   4.3 tenu~ not mino~ male   english     59          87.5           35
  7   2.8 tenu~ not mino~ male   english     59          88.6           39
- 8   4.1 tenu~ not mino~ male   english     51         100             55
+ 8   4.1 tenu~ not mino~ male   english     51           100           55
  9   3.4 tenu~ not mino~ male   english     51          56.9          111
 10   4.5 tenu~ not mino~ female english     40          87.0           40
 # ... with 453 more rows, and 13 more variables: cls_students <int>,
 #   cls_level <fct>, cls_profs <fct>, cls_credits <fct>, bty_f1lower <int>,
 #   bty_f1upper <int>, bty_f2upper <int>, bty_m1lower <int>, bty_m1upper <int>,
 #   bty_m2upper <int>, bty_avg <dbl>, pic_outfit <fct>, pic_color <fct>
+
+
+cat_vars <- droplevels(cat_vars$score, cat_vars$rank, cat_var$age, cat_var$cls_students, cat_var$bty_avg, cat_var$)
