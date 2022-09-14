69b899e436a37054621b02cecd844de060246c18
JingshuZ <91241158+mmmzzss@users.noreply.github.com>
2022-09-14 14:28:46 -0400

    Update 3. Importing Excel data.r

--- Introduction to Importing Data in R/3. Importing Excel data.r
+++ Introduction to Importing Data in R/3. Importing Excel data.r
@@ -86,9 +86,39 @@ result:
 
 
 
+# Column names for urban_pop
+columns <- c("country", paste0("year_", 1967:1974))
+
+# Finish the read.xls call
+urban_pop <- read.xls("urbanpop.xls", sheet = 2,
+                      skip = 50, header = FALSE , stringsAsFactors = FALSE,
+                      col.names = columns)
+
+# Print first 10 observation of urban_pop
+head(urban_pop, n = 10)
 
 
 
+
+
+
+# Add code to import data from all three sheets in urbanpop.xls
+path <- "urbanpop.xls"
+urban_sheet1 <- read.xls(path, sheet = 1, stringsAsFactors = FALSE)
+urban_sheet2 <- read.xls(path, sheet = 2, stringsAsFactors = FALSE)
+urban_sheet3 <- read.xls(path, sheet = 3, stringsAsFactors = FALSE)
+
+
+# Extend the cbind() call to include urban_sheet3: urban 合并并且去掉第一列
+urban <- cbind(urban_sheet1, urban_sheet2[-1], urban_sheet3[-1])
+
+
+# Remove all rows with NAs from urban: urban_clean 清除所有的NAs
+urban_clean <- na.omit(urban)
+
+
+# Print out a summary of urban_clean
+summary(urban_clean)
 
 
 
